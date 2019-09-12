198f8128abc25dc28cd8dc2007152dddc08f0013
Immo Landwerth <immol@microsoft.com>
2019-09-11 18:10:09 -0700

    Make build use Dev16

--- build.cmd
+++ build.cmd
@@ -3,8 +3,8 @@ setlocal
 
 :: Find MSBuild to use
 
-for /f "tokens=*" %%i in ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -property installationPath') do set VSPATH=%%i
-set MSBUILD_PATH=%VSPATH%\MSBuild\15.0\Bin\MSBuild.exe
+for /f "tokens=*" %%i in ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -property installationPath -prerelease') do set VSPATH=%%i
+set MSBUILD_PATH=%VSPATH%\MSBuild\Current\Bin\MSBuild.exe
 
 :: Note: We've disabled node reuse because it causes file locking issues.
 ::       The issue is that we extend the build with our own targets which
